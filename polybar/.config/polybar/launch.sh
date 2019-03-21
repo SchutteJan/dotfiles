ffd1a884e4fc0be3ab79035baa0a5031d1b09deb
Jan Schutte <jan.o.schutte@gmail.com>
2019-03-21 12:25:11 +0100

    Add polybar to second monitor

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,5 +6,11 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+MONITORS="$(xrandr -q | grep " connected" | wc -l)"
+
 # Launch bar
 polybar mybar &
+
+if [[ $MONITORS == "2" ]]; then
+	polybar mybar2 &
+fi

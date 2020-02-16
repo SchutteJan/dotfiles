b72dd28f047ee68deb72f506db41714cbd6c026f
Jan Schutte <jan.o.schutte@gmail.com>
2020-02-16 22:54:01 +0100

    Various changes

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -15,9 +15,13 @@ alias ll "lsd -la"
 # Add to path
 set PATH /home/jan/apps/node-v10.15.1-linux-x64/bin $PATH
 set PATH /home/jan/apps/flutter/bin $PATH
+set PATH /home/jan/apps/miniconda3/bin $PATH
 
 # Fuck command
 thefuck --alias | source
 
 # Disable virtual env custom prompt
 export VIRTUAL_ENV_DISABLE_PROMPT=true
+
+export EDITOR=vim
+
--- vim/.vimrc
+++ vim/.vimrc
@@ -1,6 +1,9 @@
-:set tabstop=4
+set tabstop=8 softtabstop=0 expandtab shiftwidth=4 smarttab
 
-set  rtp+=/usr/local/lib/python3.7/site-packages/powerline/bindings/vim
+set rtp+=/usr/local/lib/python3.7/site-packages/powerline/bindings/vim
 set laststatus=2
 set t_Co=256
+
+set autoindent
+set smartindent
 

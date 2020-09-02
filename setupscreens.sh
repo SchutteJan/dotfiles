c178506a7cc5f365f925706a9c988854286985bd
Jan Schutte <jan.o.schutte@gmail.com>
2020-09-02 17:21:21 +0200

    Add 'Monitor Only' option in setupscreens

--- setupscreens.sh
+++ setupscreens.sh
@@ -1,4 +1,4 @@
-options=('Laptop only' 'Monitor + Laptop' 'TV only' 'TV + Laptop' 'Automatic')
+options=('Laptop only' 'Monitor + Laptop' 'TV only' 'TV + Laptop' 'Monitor Only' 'Automatic')
 dmenuCmd='dmenu -i -l 10 -sb "#a54242"'
 
 
@@ -8,7 +8,7 @@ optionStr=${optionStr:1}
 selection=$(echo "$optionStr" | eval $dmenuCmd) 
 
 if [[ $selection == ${options[0]} ]]; then
-    xrandr --output HDMI1 --off --output DP1 --off --output eDP1 --fb 2560x1440 --mode 2560x1440 --dpi 168
+    xrandr --output HDMI1 --off --output DP1 --off --output eDP1 --fb 2560x1440 --mode 2560x1440 --dpi 168 
 elif [[ $selection == ${options[1]} ]]; then
     xrandr --fb 3584x3456 --output eDP1 --mode 2560x1440 --pos 128x2016 --dpi 168 --output HDMI1 --mode 2560x1440 --pos 0x0 --scale 1.4x1.4
 elif [[ $selection == ${options[2]} ]]; then
@@ -16,6 +16,8 @@ elif [[ $selection == ${options[2]} ]]; then
 elif [[ $selection == ${options[3]} ]]; then
     xrandr --fb 7040x3960 --output HDMI1 --mode 3840x2160 --output eDP1 --mode 3200x1800 --pos 248x2160
 elif [[ $selection == ${options[4]} ]]; then
+    xrandr --fb 3584x2016 --output HDMI1 --mode 2560x1440 --scale 1.4x1.4 --pos 0x0 --output eDP1 --off
+elif [[ $selection == ${options[5]} ]]; then
     xrandr --auto
 fi
 

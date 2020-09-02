8f7390da502fd6d4d7fa0eb7dadab8e9c6065678
Jan Schutte <jan.o.schutte@gmail.com>
2020-09-02 16:00:28 +0200

    Lower laptop screen resolution from 3200x1800 to 2560x1440

--- setupscreens.sh
+++ setupscreens.sh
@@ -8,11 +8,9 @@ optionStr=${optionStr:1}
 selection=$(echo "$optionStr" | eval $dmenuCmd) 
 
 if [[ $selection == ${options[0]} ]]; then
-    xrandr --output HDMI1 --off --output DP1 --off --output eDP1 --fb 3200x1800 --mode 3200x1800
-#elif [[ $selection == ${options[1]} ]]; then
-#    xrandr --fb 3584x3816 --output eDP1 --mode 3200x1800 --pos 128x2016 --output DP1 --mode 2560x1440 --pos 0x0 --scale 1.4x1.4
+    xrandr --output HDMI1 --off --output DP1 --off --output eDP1 --fb 2560x1440 --mode 2560x1440 --dpi 168
 elif [[ $selection == ${options[1]} ]]; then
-    xrandr --fb 3584x3816 --output eDP1 --mode 3200x1800 --pos 128x2016 --output HDMI1 --mode 2560x1440 --pos 0x0 --scale 1.4x1.4
+    xrandr --fb 3584x3456 --output eDP1 --mode 2560x1440 --pos 128x2016 --dpi 168 --output HDMI1 --mode 2560x1440 --pos 0x0 --scale 1.4x1.4
 elif [[ $selection == ${options[2]} ]]; then
     xrandr --fb 3840x2160 --output HDMI1 --mode 3840x2160 --output eDP1 --off
 elif [[ $selection == ${options[3]} ]]; then
